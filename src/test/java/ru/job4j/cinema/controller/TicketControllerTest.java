cb847fb7a0aa742327d47719ade86620d0cf05b6
Yurchenkopi <heppie@yandex.ru>
2024-04-04 16:11:36 +0300

    Controller tests upd v0.4

--- src/test/java/ru/job4j/cinema/controller/TicketControllerTest.java
+++ src/test/java/ru/job4j/cinema/controller/TicketControllerTest.java
@@ -3,15 +3,20 @@ package ru.job4j.cinema.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.ui.ConcurrentModel;
 import ru.job4j.cinema.dto.TicketDto;
+import ru.job4j.cinema.model.Ticket;
 import ru.job4j.cinema.service.TicketService;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -42,7 +47,73 @@ public class TicketControllerTest {
         var actualTicket = model.getAttribute("ticket");
 
         assertThat(view).isEqualTo("tickets/one");
-        assertThat(actualTicket).isEqualTo(expectedTicketDto);
+        assertThat(actualTicket).usingRecursiveComparison().isEqualTo(expectedTicketDto);
     }
+
+    @Test
+    public void whenRequestTicketByUserPageThenGetTicketByUserPage() {
+        var currenDate = LocalDateTime.now();
+        var ticketDto1 = new TicketDto(
+                1, "Название 1", 0, "Малый",
+                currenDate.with(LocalTime.of(12, 0)), currenDate.with(LocalTime.of(15, 40)),
+                5, 10);
+        var ticketDto2 = new TicketDto(
+                1, "Название 2", 0, "Большой>",
+                currenDate.with(LocalTime.of(12, 0)), currenDate.with(LocalTime.of(15, 40)),
+                6, 15);
+        var expectedTicketDtoList = List.of(ticketDto1, ticketDto2);
+
+        when(ticketService.findByUser(anyInt())).thenReturn(expectedTicketDtoList);
+
+        var model = new ConcurrentModel();
+        var view = ticketController.getByUsers(model, 1);
+        var actualTicketList = model.getAttribute("tickets");
+
+        assertThat(view).isEqualTo("tickets/byUsers");
+        assertThat(actualTicketList).usingRecursiveComparison().isEqualTo(expectedTicketDtoList);
+    }
+
+    @Test
+    public void whenPostTicketThenSameDataAndMessageSuccess() throws Exception {
+        var currenDate = LocalDateTime.now();
+        var expectedTicket = new Ticket(1, 5, 10, 1);
+        var expectedMessage = String.format(
+                "Поздравляем!%sВы приобрели билет на ряд-%d, место-%d.",
+                System.lineSeparator(),
+                expectedTicket.getRowNumber(), expectedTicket.getPlaceNumber()
+        );
+        var ticketDto = new TicketDto(
+                1, "Название", 0, "Малый",
+                currenDate.with(LocalTime.of(12, 0)), currenDate.with(LocalTime.of(15, 40)),
+                expectedTicket.getRowNumber(), expectedTicket.getPlaceNumber());
+        var ticketArgumentCaptor = ArgumentCaptor.forClass(Ticket.class);
+        when(ticketService.buy(ticketArgumentCaptor.capture())).thenReturn(Optional.of(ticketDto));
+
+        var model = new ConcurrentModel();
+        var view = ticketController.buyTicket(expectedTicket, model);
+
+        var actualTicket = ticketArgumentCaptor.getValue();
+        var actualMessage = model.getAttribute("message");
+
+        assertThat(view).isEqualTo("messages/message");
+        assertThat(actualTicket).usingRecursiveComparison().isEqualTo(expectedTicket);
+        assertThat(actualMessage).isEqualTo(expectedMessage);
+    }
+
+    @Test
+    public void whenTryToBuyTicketThenThrowException() throws Exception {
+        var expectedException = new Exception("Не удалось купить билет. Место уже занято.");
+        var expectedTicket = new Ticket(1, 5, 10, 1);
+
+        when(ticketService.buy(any(Ticket.class))).thenThrow(expectedException);
+
+        var model = new ConcurrentModel();
+        var view = ticketController.buyTicket(expectedTicket, model);
+        var actualMessage = model.getAttribute("message");
+
+        assertThat(view).isEqualTo("errors/404");
+        assertThat(actualMessage).isEqualTo(expectedException.getMessage());
+    }
+
 
 }
